07f0a48295937faf122cd85d633922f28fd6b49c
David Cote <dmc1991@icloud.com>
2024-04-23 23:09:15 -0400

    Added change listener to otherJobRole

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@ const shirtColor = document.getElementById(`color`);
 
 nameInput.focus();
 
+otherJobRole.style.display = `none`;
+
+// Displays the "Other Job Role" field only when the user selects the "Other" option in the "Job Role" menu.
+// If the user selects another option, after having selected the "Other" option,
+// the "Other job role" field is hidden from view once again.
+jobRoleMenu.addEventListener(`change`, (e)=>{
+  const selectedJob = e.target.value;
+  if (selectedJob === `other`){
+    otherJobRole.style.display = `block`;
+  } else {
+    otherJobRole.style.display = `none`;
+  }
+});
+
