d03d980fc48da779909079d0fcf81d4e3e562113
David Cote <dmc1991@icloud.com>
2024-04-23 23:08:42 -0400

    Added default focus to nameInput

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const nameInput = document.getElementById(`name`);
+const jobRoleMenu = document.getElementById(`title`);
+const otherJobRole = document.getElementById(`other-job-role`);
+const shirtDesign = document.getElementById(`design`);
+const shirtColor = document.getElementById(`color`);
+
+nameInput.focus();
+
